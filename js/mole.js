0540a96076de5777daf2ae87cc049167459c4295
Ashish0077 <ashish007722@gmail.com>
2020-01-18 16:57:41 +0530

    [mole.js] getInterval() calculating interval for updation in seperate function
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- js/mole.js
+++ js/mole.js
@@ -2,65 +2,69 @@
 const moles = [
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-0")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-1")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-2")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-3")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-4")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-5")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-6")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-7")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-8")
     },
     {
         status: "sad",
-        next: Date.now() + 1000,
+        next: getInterval(),
         king: false,
         node: document.querySelector("#hole-9")
     },
 ]
+
+function getInterval() {
+    return Date.now() + 1000;
+}
 
 function init() {
 
