fde1f7efdb76851c2403780d8af32ab3a823fce6
Ashish0077 <ashish007722@gmail.com>
2020-01-18 16:06:10 +0530

    update mole.js representation of moles
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

---
+++ js/mole.js
@@ -0,0 +1,69 @@
+//list of all the moles representing their state
+const moles = [
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-0")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-1")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-2")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-3")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-4")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-5")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-6")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-7")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-8")
+    },
+    {
+        status: "sad",
+        next: Date.now() + 1000,
+        king: false,
+        node: document.querySelector("#hole-9")
+    },
+]
+
+function init() {
+
+}
+
+init();
